8cc0d446dd150a97d4d4fb284e80b540b47214fc
Gabriel Feitosa <gabrielfeitosacristovam@gmail.com>
2023-04-29 16:00:17 -0300

    Criação da página inicial e  main do app

---
+++ lib/2-app/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:projeto/2-app/modules/home/pages/home_page.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: "Projeto Animais",
+    ),
+  );
+}
--- lib/2-app/modules/home/pages/home_page.dart
+++ lib/2-app/modules/home/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +9,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleIndexChanged(int i) {
+    setState(() {
+      _selectedTab = _SelectedTab.values[i];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Projeto Animais"),
+        shadowColor: Colors.transparent,
+      ),
+      extendBody: true,
+      body: Container(),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: DotNavigationBar(
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          dotIndicatorColor: Colors.white,
+          unselectedItemColor: Colors.grey[300],
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: const Icon(Icons.home),
+              selectedColor: const Color(0xff73544C),
+            ),
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: const Icon(Icons.favorite),
+              selectedColor: const Color(0xff73544C),
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: const Icon(Icons.search),
+              selectedColor: const Color(0xff73544C),
+            ),
+
+            /// Profile
+            DotNavigationBarItem(
+              icon: const Icon(Icons.person),
+              selectedColor: const Color(0xff73544C),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+enum _SelectedTab { home, favorite, search, person }
